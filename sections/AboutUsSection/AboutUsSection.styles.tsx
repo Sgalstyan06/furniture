c389eefd16662a9d474a746da375f8a1afe98679
Sgalstyan06 <96201224+Sgalstyan06@users.noreply.github.com>
2022-10-14 17:26:59 +0400

    try something

--- sections/AboutUsSection/AboutUsSection.styles.tsx
+++ sections/AboutUsSection/AboutUsSection.styles.tsx
@@ -2,16 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   margin: 100px 30px;
+`;
+export const ImageWrapper = styled.div``;
+export const About = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 10px;
 `;
-export const ImageWrapper = styled.div``;
-export const Info = styled.div``;
 export const Title = styled.h1`
-  font-size: 30px;
-  font-weight: 500;
-  line-height: 2em;
+  font-size: 34px;
+  font-weight: 700;
+  line-height: 1.2;
+  text-align: center;
+  margin-bottom: 20px;
 `;
 export const Description = styled.h2`
   font-size: 22px;
--- sections/AboutUsSection/AboutUsSection.tsx
+++ sections/AboutUsSection/AboutUsSection.tsx
@@ -5,25 +5,26 @@ import * as Styled from "./AboutUsSection.styles";
 const AboutUsSection = () => {
   return (
     <Styled.Container>
-      <Styled.ImageWrapper>
-        <Image
-          src="/about/about.jpg"
-          width={100}
-          height={100}
-          layout="responsive"
-          objectFit="cover"
-          alt="about"
-        />
-      </Styled.ImageWrapper>
-      <Styled.Info>
-        <Styled.Title>О нас</Styled.Title>
+      <Styled.Title>О Armen нас</Styled.Title>
+      <Styled.About>
+        <Styled.ImageWrapper>
+          <Image
+            src="/about/about.jpg"
+            width={100}
+            height={100}
+            layout="responsive"
+            objectFit="cover"
+            alt="about"
+          />
+        </Styled.ImageWrapper>
+
         <Styled.Description>
           Магазины Arstilbeko принадлежат ООО Эд-СПО Сеть наших мебельных
           салонов в Армении действует уже 14 лет и предлагает клиентам,
           оценивающим качество, стильную и качественную импортную мебель
           польского, европейского и российского производства
         </Styled.Description>
-      </Styled.Info>
+      </Styled.About>
     </Styled.Container>
   );
 };
